82c4cf8f6855b88ae4b82121b6f52d8ab976210f
Gonçalo Rosa <goncalojmrosa@gmail.com>
2024-05-03 17:34:44 +0100

    MongoDB setup

---
+++ back/database/database.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func goDotEnvVariable(key string) string {
+  err := godotenv.Load(".env")
+
+  if err != nil {
+    log.Fatalf("Error loading .env file")
+  }
+
+  return os.Getenv(key)
+}
+
+var db *mongo.Client
+
+func Connect() *mongo.Database {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	url := goDotEnvVariable("DATABASE_URL")
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(url))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	return client.Database("Cluster0")
+}
---
+++ back/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+  r := mux.NewRouter()
+  r.HandleFunc("/hello", helloHandler).Methods("GET")
+  http.Handle("/", r)
+
+  log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
